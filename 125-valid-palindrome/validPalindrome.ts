5f09cfab9ad3d6dd0a987ba59b3f32d6f26e3437
Jeff <jbridges1119@gmail.com>
2022-10-24 08:51:13 -0600

    Add second method to solve

--- 125-valid-palindrome/validPalindrome.ts
+++ 125-valid-palindrome/validPalindrome.ts
@@ -11,6 +11,16 @@ function isPalindrome(s: string): boolean {
   }
   return true;
 };
+//Second method
+function isPalindrome1(s: string): boolean {
+  //set char to lower case and then use replace to remove all but chars
+  const lowerS = s.toLowerCase();
+  const charString = lowerS.replace(/[^a-z0-9]/gi, '');
+  const reverseString = charString.split("").reverse().join('');
+  if (charString === reverseString) return true;
+  return false;
+};
 
 
-console.log(isPalindrome(" "));
+
+console.log(isPalindrome1("racecar"));
